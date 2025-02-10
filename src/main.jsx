0b8e02f8bd61714230af5295131433425f3177d0
vipulbhol26 <vipulbhole121@gmail.com>
2025-02-10 18:47:26 +0530

    error resolve

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
